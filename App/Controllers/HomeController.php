dc070c326cf59bff56f70ea54dc4aeff016aaa8a
Arturo Soberanes <arturo.soberanes01@gmail.com>
2023-06-18 22:35:32 -0600

    simple_html_dom added and functionality for check information of product in amazon added

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -1,10 +1,35 @@
 <?php
 namespace App\Controllers;
 
+use simplehtmldom\HtmlDocument;
+use simplehtmldom\HtmlWeb;
+
 class HomeController
 {
+
   public function show()
   {
+    $clientWeb = new HtmlWeb;
+    $clientHtml = new HtmlDocument();
+    $html = $clientWeb->load('https://www.amazon.com/s?k=keyboard');
+    $allNodes = $html->find('div[data-component-type=s-search-result]');
+
+    $results = [];
+    foreach ($allNodes as $node) {
+      $productNode = $clientHtml->load($node->innertext);
+      $image = $productNode->find('img', 0);
+      $title = $productNode->find('h2', 0);
+      $url = $productNode->find('h2 a', 0);
+      $price = $productNode->find('.a-offscreen', 0);
+      $results[] = [
+        'image' => $image->src,
+        'title' => $title->plaintext,
+        'url' => $url->href,
+        'price' => $price ? $price->plaintext : null
+      ];
+    }
+    dd($results);
+
     return view('index');
   }
 }
